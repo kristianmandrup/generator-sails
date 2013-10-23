23379908d6ba9f57b548daad4ea3a6f9f05064ce
Kristian Mandrup <kmandrup@gmail.com>
2013-10-23 22:42:25 +0200

    using sails generators under the hood

--- controller/index.js
+++ controller/index.js
@@ -1,5 +1,8 @@
 'use strict';
 var util = require('util');
+var rimraf = require('rimraf');
+var child_process = require('child_process');
+var spawn = child_process.spawn;
 var yeoman = require('yeoman-generator');
 
 var ControllerGenerator = module.exports = function ControllerGenerator(args, options, config) {
@@ -10,9 +13,11 @@ util.inherits(ControllerGenerator, yeoman.generators.NamedBase);
 
 ControllerGenerator.prototype.files = function files() {
   var command, child;
-  command = 'sails new controller ' + this.name;
+  command = 'sails generate controller ' + this.name;
 
   console.log(command);
+
+  var self = this;
 
   child = child_process.exec(command, function (error, stdout, stderr) {
     if (error) {
@@ -21,12 +26,13 @@ ControllerGenerator.prototype.files = function files() {
        console.log('Signal received: '+error.signal);
     }
     if (stdout) {
-      console.log('Sails controller ' + this.name + ' created successfully :)');
+      console.log('Sails controller ' + self.name + ' created successfully :)');
       console.log('Note: The only thing better than thin controllers is no controllers.');
     }
   });
 
   child.on('exit', function (code) {
-    console.log('Child process exited with exit code '+code);
+    if (code != 0)
+      console.log('Child process exited with exit code '+code);
   });  
 };
--- model/index.js
+++ model/index.js
@@ -1,5 +1,8 @@
 'use strict';
 var util = require('util');
+var rimraf = require('rimraf');
+var child_process = require('child_process');
+var spawn = child_process.spawn;
 var yeoman = require('yeoman-generator');
 
 var ModelGenerator = module.exports = function ModelGenerator(args, options, config) {
@@ -10,7 +13,11 @@ util.inherits(ModelGenerator, yeoman.generators.NamedBase);
 
 ModelGenerator.prototype.files = function files() {
   var command, child;
-  command = 'sails new model ' + this.name;
+  command = 'sails generate model ' + this.name;
+
+  console.log(command);
+
+  var self = this;
 
   child = child_process.exec(command, function (error, stdout, stderr) {
     if (error) {
@@ -19,11 +26,12 @@ ModelGenerator.prototype.files = function files() {
        console.log('Signal received: '+error.signal);
     }
     if (stdout) {
-      console.log('Sails model ' + this.name + ' created successfully :)');
+      console.log('Sails model ' + self.name + ' created successfully :)');
     }
   });
 
   child.on('exit', function (code) {
-    console.log('Child process exited with exit code '+code);
+    if (code != 0)
+      console.log('Child process exited with exit code '+code);
   });  
 };
